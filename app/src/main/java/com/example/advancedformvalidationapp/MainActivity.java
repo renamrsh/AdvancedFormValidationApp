c6a42c9ab941f571484115ce26748d252e8ba692
Iryna Mariash <9602106u@qed.local>
2024-09-19 13:26:34 +0200

    Initial commit

---
+++ app/src/main/java/com/example/advancedformvalidationapp/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.advancedformvalidationapp;
+
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextImie;
+    private EditText editTextNazwisko;
+    private EditText editTextEmail;
+    private EditText editTextNnumerTelefonu;
+    private EditText editTextHaslo;
+    private EditText editTextPodtwHasla;
+    private Button buttonWyslij;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextImie = findViewById(R.id.Imie);
+        editTextNazwisko = findViewById(R.id.Nazwisko);
+        editTextEmail = findViewById(R.id.Email);
+        editTextNnumerTelefonu = findViewById(R.id.NumerTelefonu);
+        editTextHaslo = findViewById(R.id.Haslo);
+        editTextPodtwHasla = findViewById(R.id.PodtwHasla);
+        buttonWyslij = findViewById(R.id.Przycisk);
+
+        buttonWyslij.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String imie = editTextImie.getText().toString().trim();
+                String nazwisko = editTextNazwisko.getText().toString().trim();
+                String email = editTextEmail.getText().toString().trim();
+                String numberTelefonu = editTextNnumerTelefonu.getText().toString().trim();
+                String haslo = editTextHaslo.getText().toString().trim();
+                String podtwHasla = editTextPodtwHasla.getText().toString().trim();
+
+                if(imie.isEmpty()||nazwisko.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Prosze wprowadzić Imie i Nazwisko", Toast.LENGTH_SHORT).show();
+                }else if(email.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Prosze wprowadzić adres e-mail", Toast.LENGTH_SHORT).show();
+                }else if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    Toast.makeText(MainActivity.this, "Niepoprawny adres e-mail", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Formularz przeslany poprawnie", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
